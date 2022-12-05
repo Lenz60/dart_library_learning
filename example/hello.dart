e3957bd97bcfea2ece821e316dda608e87c09538
Rafly Andrian <52058310+Lenz60@users.noreply.github.com>
2022-12-05 22:57:55 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_library/hello.dart';
+
+void main(){
+  print(sayHello("Rapli"));
+
+  Customer customer = Customer();
+  Product product = Product();
+  Category category = Category();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library dart_library;
+
+export 'src/say_hello.dart' show sayHello; //only say hello function that exported
+export 'src/customer.dart' show Category, Product, Customer;
